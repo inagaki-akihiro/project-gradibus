15466631970522e83b4201fe4bc16c0cf4912868
inagaki-akihiro <gakihiro20020202@gmail.com>
2021-02-09 10:22:44 +0900

    Login

---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login">
+      <p>アカウントをお持ちでない方はこちら</p>
+
+　　
+　　 <!-- ここにvue-routerで新規作成ページを繋ぐ -->
+　　
+   <input v-model = "mailValue" placeholder = "メールアドレス">
+   <input v-model = "passwordValue" placeholder = "パスワード">
+   <button @click="click">ログイン</button> 
+  </div>
+</template>
+
+<script>
+//import axios from 'axios'
+export default {
+    data(){
+        return{
+            mailValue : "",
+            passwordValue : ""
+            }
+            },
+         methods:{
+               click(){
+    console.log(1)
+                }
+     }
+ }
+</script>
+
+<style>
+
+</style>
