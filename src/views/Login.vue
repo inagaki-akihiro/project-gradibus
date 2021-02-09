67c62e4469448a35a43c785901522bf3329cb052
inagaki-akihiro <gakihiro20020202@gmail.com>
2021-02-09 12:29:25 +0900

    create ui about login and signup

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="login">
-    <router-link to="/signup">Signup</router-link>
-    <p>アカウントをお持ちでない方はこちら</p>
+      <p>アカウントをお持ちでない方はこちら</p>
+    <router-link to="/signup"><button class="signupButton">新規作成</button></router-link>
+    
 <!-- ここにvue-routerで新規作成ページを繋ぐ -->
     <div>
     <router-view/>
      </div>
-   <input v-model = "mailValue" placeholder = "メールアドレス">
-   <input v-model = "passwordValue" placeholder = "パスワード">
-   <button @click="click">ログイン</button> 
+     
+   <input  class="loginInput" v-model = "mailValue" placeholder = "メールアドレス">
+   <br>
+   <input  class="loginInput" v-model = "passwordValue" placeholder = "パスワード">
+   <br>
+   <button class="loginButton" @click="click">ログイン</button> 
   </div>
 </template>
 
@@ -30,5 +34,23 @@ export default {
 </script>
 
 <style>
+.signupButton {
+    margin-bottom:50px;
+     width:500px;
+    height:40px;
+}
+
+.loginButton{
+    width:500px;
+    height:40px;
+}
+
+.loginInput{
+    box-sizing: border-box;
+    width:500px;
+    height:40px;
+    margin-bottom:30px;
+}
+
 
 </style>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="signup">
-    <h1>This is an about page</h1>
+   <h2>会員情報入力</h2>
+
+   <h4>ニックネーム<span class="mustSpan">必須</span></h4> 
+
+   <input class="loginInput" v-model="nameInput" placeholder="（例）メルカリ太郎">
+
+   <h4>メールアドレス<span class="mustSpan">必須</span></h4>
+   <input class="loginInput" v-model="mailInput" placeholder="PC・携帯どちらでも可">
+
+   <h4>パスワード<span class="mustSpan">必須</span></h4>
+   <input class="loginInput" v-model="passwordInput" placeholder="7文字以上の半角英数字">
+  <br>
+  <input class="checkbox" type="checkbox">
+  <span>パスワードを表示する</span>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.mustSpan{
+  color:white;
+  background-color:red;
+}
+.checkbox{
+  width:initial;
+  height:initial;
+}
+</style>
